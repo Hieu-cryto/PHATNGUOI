a03f8a0fd301bc5630b0d6b817d9ac469417a7b0
Hieu-cryto <hieuminhle0408@gmail.com>
2025-05-12 23:16:28 +0700

    phatnguoi

---
+++ phatnguoi.py
@@ -0,0 +1,47 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+from PIL import Image
+import pytesseract
+from io import BytesIO
+import schedule
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"   
+
+def PhatNguoi():
+    driver = webdriver.Chrome()
+    driver.get("https://www.csgt.vn/tra-cuu-phuong-tien-vi-pham.html")
+    click_bien_kiem_soat = '#formBSX > div.bot > div:nth-child(1) > input'    
+    element_bien_kiem_soat= driver.find_element(By.CSS_SELECTOR, click_bien_kiem_soat)
+    element_bien_kiem_soat.send_keys("17A43331")
+
+    click_phuong_tien = '#formBSX > div.bot > div:nth-child(2) > select > option:nth-child(1)'    
+    element_phuong_tien = driver.find_element(By.CSS_SELECTOR, click_phuong_tien)
+    element_phuong_tien.click()
+
+    capcha_element = driver.find_element(By.CSS_SELECTOR, '#imgCaptcha')
+    capcha_png = capcha_element.screenshot_as_png
+
+    image = Image.open(BytesIO(capcha_png))
+
+    capcha_text = pytesseract.image_to_string(image, config='--psm 8').strip()
+    new_text1 = capcha_text[1:]
+    new_text2 = new_text1.replace(" ", "")
+      
+    time.sleep(2)
+    click_bien_kiem_soat = '#formBSX > div.bot > div:nth-child(3) > div > input'    
+    element_bien_kiem_soat= driver.find_element(By.CSS_SELECTOR, click_bien_kiem_soat)
+    element_bien_kiem_soat.send_keys(new_text2)
+    time.sleep(2)
+    click_xe = '#formBSX > div.bot > input.btnTraCuu'    
+    element_xe = driver.find_element(By.CSS_SELECTOR, click_xe)
+    element_xe.click()
+
+schedule.every().day.at("06:00").do(PhatNguoi)
+schedule.every().day.at("12:00").do(PhatNguoi)
+
+print('Đang thực hiện ...')
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)  
